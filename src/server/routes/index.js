b2290738e4d1972fdca0a8dcc0e45e0bafd3d94e
Louisa Reese <louisa.e.reese@gmail.com>
2016-03-24 14:41:29 -0600

    redirect to user page works

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -12,18 +12,18 @@ router.get('/facebook', passport.authenticate('facebook'));
 router.get('/facebook/callback', passport.authenticate('facebook', {
   failureRedirect: '/',
 }), function (req, res, next) {
-  res.redirect('/');
+  res.redirect('/user/' + req.user);
 });
 
 router.get('/logout', function (req, res, next) {
-  req.session.destroy();
+  req.session = null;
   req.logOut();
   res.redirect('/');
 });
 
 router.get('/user/:id', function (req, res, next) {
   queries.getSingleUser(req.params.id).then(function (result) {
-    res.render('userProfile', { user: result[0] });
+    res.render('userProfile', { user: result[0], stylesheet: 'main.css' });
   });
 });
 
